7bf0ecd41ff4c87d8b2914cec45573f051d83ec0
TREVTAL <trevtal@gmail.com>
2022-09-10 18:41:59 -0500

    delete note coded

--- Develop/server.js
+++ Develop/server.js
@@ -28,46 +28,60 @@ app.get('/api/notes', (req, res) => {
 
 //POST request for save btn
 app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a note`);
+            // Log that a POST request was received
+            console.info(`${req.method} request received to add a note`);
 
-    // Destructuring items in req.body
-    const { title, text } = req.body;
-  
-    // If all properties are present
-    if (title && text ) {
-      // Variable for the object to save
-      const newNote = {
-        title,
-        text,
-        id: uuid(),
-      };
+            // Destructuring items in req.body
+            const { title, text } = req.body;
+        
+            // If all properties are present
+            if (title && text ) {
+            // Variable for the object to save
+            const newNote = {
+                title,
+                text,
+                id: uuid(),
+            };
 
-    let rawData = fs.readFileSync(`./db/db.json`);
-    let parsedData = JSON.parse(rawData)
+            let rawData = fs.readFileSync(`./db/db.json`);
+            let parsedData = JSON.parse(rawData)
 
-    parsedData.push(newNote);
-    newData = JSON.stringify(parsedData, null, 2);      
-    
-    //write string
-    fs.writeFileSync(`./db/db.json`, newData);
-    console.log(`New Note has been written to JSON file`);
+            parsedData.push(newNote);
+            newData = JSON.stringify(parsedData, null, 2);      
+            
+            //write string into db.json
+            fs.writeFileSync(`./db/db.json`, newData);
+            console.log(`New Note has been written to JSON file`);
 
-    const response = {
-        status: 'success',
-        body: newNote,
-    };
-  
-    //   console.log(response);
-      res.status(201).json(response);
-    } else {
-      res.status(500).json('Error in posting review');
-    }
+            const response = {
+                status: 'success',
+                body: newNote,
+            };
+        
+            res.status(201).json(response);
+            } else {
+            res.status(500).json('Error in posting review');
+            }
   });
 
 
 
+  app.delete('/api/notes/:id' , (req, res) => {
+    console.info(`${req.method} request received to delete note`);
+    
+    // fetch data from db.json
+    let rawData = fs.readFileSync(`./db/db.json`);
+    let parsedData = JSON.parse(rawData);
+    
+    // remove object with ID from parsed data
+    let parsedRemainingData = parsedData.filter( data => data.id != req.params.id);
+    let remainingData = JSON.stringify(parsedRemainingData, null, 2);
+    fs.writeFileSync(`./db/db.json`, remainingData);
+   
+    res.send('Note Deleted');
 
+
+  });
 
 
 
