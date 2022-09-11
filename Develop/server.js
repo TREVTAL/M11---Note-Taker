ab9a9c3e502eb58e9921d96ed06b136b16ad4288
TREVTAL <trevtal@gmail.com>
2022-09-10 19:12:24 -0500

    added procfile and setup PORT

--- Develop/server.js
+++ Develop/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const uuid = require('./public/assets/helpers/uuid');
 // const db = require('./db/db.json');
 const fs = require('fs');
-const PORT = 3001 ;
+const PORT = process.env.PORT || 3001 ;
 
 const app = express();
 
